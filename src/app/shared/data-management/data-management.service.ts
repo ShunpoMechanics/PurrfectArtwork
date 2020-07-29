cfb746ca29c87175e1c63affe179463cf7c9729f
Jonathan Correa <jonathanc@gridsystems.net>
2020-07-29 10:00:06 -0400

    Removed unused import

--- src/app/shared/data-management/data-management.service.ts
+++ src/app/shared/data-management/data-management.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { environment } from "src/environments/environment";
-import { AuthService } from "src/app/auth/auth.service";
-import { take, exhaustMap, takeLast } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { take, exhaustMap } from "rxjs/operators";
 import * as firebase from "firebase/app";
 import "firebase/storage";
 
